9e27f937f6dab689148f4a08fa67319adb17faed
Philip Westrich <pwestrich@me.com>
2016-11-07 13:34:47 -0600

    We have a menu.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main(){
+
+	loop {
+
+		println!("Menu");
+		println!("---------------------------------------------------------------------");
+		println!("1.  Generate a random polynomial with complex coefficients.");
+		println!("2.  Read coefficients from a file.");
+		println!("3.  Write coefficients to a file.");
+		println!("4.  Evaluate the current polynomial using the naive method.");
+		println!("5.  Evaluate the current polynomial using Horner's method.");
+		println!("6.  Evaluate the current polynomial using the improved naive method");
+		println!("7.  Evaluate the current polynomial using the Fast Fourier Transform.");
+		println!("8.  Display run times for the above four algorithms.");
+		println!("9.  Display the multiplpication count for the above four algorithms.");
+		println!("10. Quit the program.\n");
+
+		println!("The current polynomial is: <SOMETHING>");
+		print!("Your choice: ");
+		break;
+
+	}
+
 }
